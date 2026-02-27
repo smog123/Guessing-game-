092208e324cda7b4a2ab462a63e86870ee0365ed
oladejumo <adejumooluwasegun35@gmail.com>
2026-02-27 11:08:18 +0000

    guessing game

--- main.go
+++ main.go
@@ -10,21 +10,30 @@ func main() {
 	secret := rand.Intn(100) + 1
 	Attempt := 0
 	maxAttempt := 5
+	Guesses := []int{}
+
+	fmt.Println("Welcome to guessing game")
 
 	for Attempt < maxAttempt {
-		fmt.Println("Welcome to guessing game")
+		//fmt.Println("Welcome to guessing game")
 		fmt.Print("Enter a Number ")
 		fmt.Scanln(&guess)
-		Attempt++
 
+		Attempt++
+		Guesses = append(Guesses, guess)
 		if guess > secret {
 			fmt.Println("Too high")
 		} else if guess < secret {
 			fmt.Println("Too low")
 		} else {
 			fmt.Println("you got it")
+			fmt.Println("Your guesses", Guesses)
+			fmt.Println("your attempts ", Attempt)
 			return
 		}
 	}
+	fmt.Println("out of attempts")
+	fmt.Println("Guesses is ", Guesses)
+	fmt.Println("the secret number is", secret)
 
 }
